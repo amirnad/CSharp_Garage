57ffb1078d63827afc88ff21d5e6cae6a96832c9
lermanori <lermanori@gmail.com>
2018-05-13 18:51:43 +0300

    follow_up

--- B18 Ex03 OriLerman 205665508 AmirNadav 305707044/B18 Ex03 Ori 205665508 Amir 305707044/Ex03.GarageLogic/DataStructure.cs
+++ B18 Ex03 OriLerman 205665508 AmirNadav 305707044/B18 Ex03 Ori 205665508 Amir 305707044/Ex03.GarageLogic/DataStructure.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    class DataStructure
+    public class DataStructure
     {
         private Dictionary<String, Vehicle> vehiclesDictionary = new Dictionary<String, Vehicle>();
         private List<Vehicle> m_fixedList = new List<Vehicle>();
