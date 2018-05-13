fc7c18563ca7d850d98823f47647cf683ce02aee
lermanori <lermanori@gmail.com>
2018-05-14 01:32:24 +0300

    the start of garageManager and the general idea of exception

---
+++ B18 Ex03 OriLerman 205665508 AmirNadav 305707044/B18 Ex03 Ori 205665508 Amir 305707044/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    /// <summary>
+    /// ArgumentOutOfRangeException class inherits Exception
+    /// </summary>
+    class ValueOutOfRangeException : Exception
+    {
+        float m_MaxValue;
+        float m_MinValue;
+        public ValueOutOfRangeException(float i_MinValue,float i_MaxValue,string i_paramName)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+
+        }
+        
+         
+    }
+}
