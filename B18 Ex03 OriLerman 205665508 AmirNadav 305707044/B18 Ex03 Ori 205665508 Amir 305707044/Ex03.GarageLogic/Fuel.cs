792792b08f6165381bfe59b070b014565713f719
amirn <amir.nadav@gmail.com>
2018-05-13 17:50:53 +0300

    Created beginnings
    Car, EnergyType, Fuel, InitialDetails

---
+++ B18 Ex03 OriLerman 205665508 AmirNadav 305707044/B18 Ex03 Ori 205665508 Amir 305707044/Ex03.GarageLogic/Fuel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Fuel : EnergyType
+    {  
+        public enum eFuelType { Octan95, Octan96, Octan98, Soler}
+        private eFuelType m_FuelType;
+        private float m_CurrentFuelAmount;
+        private readonly float r_MaxFuelAmount;
+
+        public void Refuel(eFuelType i_Type, float i_FuelAmountToAdd)
+        {
+            if(i_Type == m_FuelType)
+            {
+                if(m_CurrentFuelAmount + i_FuelAmountToAdd <= r_MaxFuelAmount)
+                {
+                    m_CurrentFuelAmount += i_FuelAmountToAdd;
+                }
+                else
+                {
+                    //throw ValueOutOfRangeException;
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
